f18d34e6eb2c8fc148b6960fb73f1003882ee6de
shivanand <shivanandstelsang@gmail.com>
2024-04-22 17:12:48 +0530

    small patch

--- src/test/java/Basics/Build_Rest_Api_Automation_Test.java
+++ src/test/java/Basics/Build_Rest_Api_Automation_Test.java
@@ -25,5 +25,6 @@ public class Build_Rest_Api_Automation_Test {
 	.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
              .header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
    System.out.println("responsebody : " + response);
+   System.out.println("api");
      }
 }
