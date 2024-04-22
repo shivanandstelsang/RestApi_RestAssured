f22a4c8e7a4c191248bb4176954e348220ed1baa
shivanand <shivanandstelsang@gmail.com>
2024-04-22 16:55:32 +0530

    restapi codes

--- src/test/java/Basics/Build_Rest_Api_Automation_Test.java
+++ src/test/java/Basics/Build_Rest_Api_Automation_Test.java
@@ -4,23 +4,26 @@ import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import org.testng.annotations.Test;
+
 import Files.Payload;
 
 public class Build_Rest_Api_Automation_Test {
-public static void main(String[] args)
-{
+
 	// validate if Add Place API is working as expected
 	//1)Given :- all input details
 	//2)When :- Submit the API - resource, http method
 	//3)Then :- Validate the response
+	@Test
+	public static void restapi() {
 	
 	RestAssured.baseURI= "https://rahulshettyacademy.com";
-	given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+	String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
 	.body(Payload.AddPlace())
 	
 	.when().post("maps/api/place/add/json")
 	.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
-             .header("Server", "Apache/2.4.52 (Ubuntu)");
-   //  System.out.println("Response Body: " + responseBody);
+             .header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
+   System.out.println("responsebody : " + response);
      }
 }
